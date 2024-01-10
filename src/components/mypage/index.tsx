724a5be34446051a50c55db2475579fdc368cc0b
ebokyung <dlqhrud303@naver.com>
2024-01-10 17:11:10 +0900

    Refactor: PostCRUDPage 요소들 컴포넌트 분리 (Image, ShowInfo, ShowResInfo)

--- src/components/mypage/index.tsx
+++ src/components/mypage/index.tsx
@@ -1,9 +1,23 @@
 import Postcode from "./admin/Postcode/Postcode";
 import ReservationForm from "./admin/ReservationForm/ReservationForm";
 import ReservationListModal from "./admin/ReservationListModal/ReservationListModal";
+import ImageInputSection from "./admin/ImageInputSection/ImageInputSection";
+import ShowInfoInputsSection from "./admin/ShowInfoInputsSection/ShowInfoInputsSection";
+import ShowResInfoInputsSection from "./admin/ShowResInfoInputsSection/ShowResInfoInputsSection";
 import ReservationItem from "./user/ReservationItem/ReservationItem";
 import ReservationUserModal from "./user/ReservationUserModal/ReservationUserModal";
 import ReviewItem from "./user/ReviewItem/ReviewItem";
 import StoryItem from "./user/StoryItem/StoryItem";
 
-export { ReservationListModal, ReservationForm, Postcode, ReviewItem, StoryItem, ReservationItem, ReservationUserModal };
+export {
+  ReservationListModal,
+  ReservationForm,
+  Postcode,
+  ReviewItem,
+  StoryItem,
+  ReservationItem,
+  ReservationUserModal,
+  ShowInfoInputsSection,
+  ShowResInfoInputsSection,
+  ImageInputSection,
+};
