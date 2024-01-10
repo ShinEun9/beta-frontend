91aee1a05897d379755e1f4865b7751dede2219a
ShinEun9 <ses2201@naver.com>
2024-01-10 13:07:20 +0900

    Refactor: 컴포넌트 return문에서 배너이미지를 합치는게 아닌, 배너이미지 배열을 만들어서 처리 (#1)

--- src/components/detail/Banner/Banner.tsx
+++ src/components/detail/Banner/Banner.tsx
@@ -6,12 +6,13 @@ import styles from "./Banner.module.css";
 
 const Banner = () => {
   const { id: showId } = useParams();
-  const { main_image_url, sub_images_url } = queryClient.getQueryData<ShowType>(["infoData", showId])!;
-  const subImgs = (sub_images_url && Object.values(JSON.parse(sub_images_url))) || [];
+  const { main_image_url: mainImg, sub_images_url } = queryClient.getQueryData<ShowType>(["infoData", showId])!;
+  const subImgs = sub_images_url ? Object.values(JSON.parse(sub_images_url)) : [];
+  const bannerImgs = [mainImg, ...subImgs];
 
   return (
     <Carousel index={0}>
-      {[main_image_url, ...subImgs].map((img, index) => (
+      {bannerImgs.map((img, index) => (
         <div key={index}>
           <img src={import.meta.env.VITE_APP_IMAGE_DOMAIN + img} className={styles["slider__img"]} />
         </div>
