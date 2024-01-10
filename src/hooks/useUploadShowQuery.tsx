aafb266c1c7edb9bba1408ef2e3234aa177e4549
ebokyung <dlqhrud303@naver.com>
2024-01-10 17:35:23 +0900

    Feat: 게시글 업로드 후 게시글 리스트 페이지로 이동

--- src/hooks/useUploadShowQuery.tsx
+++ src/hooks/useUploadShowQuery.tsx
@@ -1,14 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import { postShow } from "@/apis";
+import { useNavigate } from "react-router-dom";
 
 const useUploadShowQuery = () => {
+  const navigate = useNavigate();
   const { mutate: mutateUploadShow } = useMutation({
     mutationFn: (formData: FormData) => postShow(formData),
     onSuccess: (data) => {
       if (data) {
         toast.success("게시글 업로드 성공");
-        // navigate("/mypage/admin/post");
+        navigate("/mypage/admin/post");
       }
     },
     onError: (err) => {
