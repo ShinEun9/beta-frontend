6eacd67ab709c9fed1ce4b32b1bf1b5a9464a041
윤태현 <yoonth0919@gmail.com>
2024-01-15 15:32:25 +0900

    Feat: QueryClient defaultOptions 기능 추가 (#119)
    Co-authored-by: 신은수 <ShinEun9@users.noreply.github.com>
    Co-authored-by: bogyeong.lee <dlqhrud303@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/layouts";
 import { useModalStore } from "./stores";
 import PrivateRoute from "./PrivateRoute";
+import { useApiError } from "./hooks";
 
 function App() {
   const location = useLocation();
@@ -14,11 +16,24 @@ function App() {
     setOpenModal({ state: false, type: "" });
   }, [location.pathname, setOpenModal]);
 
+  const { handleError } = useApiError();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: handleError,
+      },
+    },
+    queryCache: new QueryCache({
+      onError: handleError,
+    }),
+  });
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {location.pathname !== "/signup" && location.pathname !== "/login" && <Header />}
       {isPrivateRoute ? <PrivateRoute /> : <Outlet />}
-    </>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   );
 }
 
