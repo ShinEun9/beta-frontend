06ecde3c906e8c9a2cc7d926904de13b4c4d3262
윤태현 <yoonth0919@gmail.com>
2024-01-17 00:16:41 +0900

    Refactor: LikeManagePage 구조 변경 (#45)

--- src/pages/Mypage/user/LikeManage/LikeManagePage.tsx
+++ src/pages/Mypage/user/LikeManage/LikeManagePage.tsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getUserLikeList } from "@/apis";
-import { BasicCard, FilterButton, TicketCard } from "@/components/common";
+import { FilterButton } from "@/components/common";
+import { LikeItem } from "@/components/mypage";
 import styles from "./LikeManagePage.module.css";
-import classNames from "classnames/bind";
-
-const cx = classNames.bind(styles);
 
 const categories = ["공연", "전시"];
 
 const LikeManagePage = () => {
-  const [filter, setFilter] = useState("공연");
+  const [showText, setShowText] = useState("공연");
 
   const handleClickFilterButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     const { textContent: value } = e.target as HTMLButtonElement;
-    setFilter(value!);
+    setShowText(value!);
   };
 
   const {
@@ -29,28 +27,16 @@ const LikeManagePage = () => {
   if (status === "pending") return <h1>loading...</h1>;
   if (status === "error") return <h1>{error.message}</h1>;
 
-  const renderLikeList = () => {
-    return (
-      <ul className={cx(filter === "전시" ? "exhibition-list" : "concert-list")}>
-        {userLikeList
-          .filter((data) => data.show_type === filter)
-          .map((item) => (
-            <li key={item.id}>{filter === "전시" ? <BasicCard item={item} /> : <TicketCard item={item} />}</li>
-          ))}
-      </ul>
-    );
-  };
-
   return (
     <div className={styles["filter-row"]}>
       <div className={styles["filter-contents"]}>
         {categories.map((item) => (
-          <FilterButton key={item} selected={filter === item} onClick={handleClickFilterButton}>
+          <FilterButton key={item} selected={showText === item} onClick={handleClickFilterButton}>
             {item}
           </FilterButton>
         ))}
       </div>
-      {renderLikeList()}
+      <LikeItem showText={showText} userLikeList={userLikeList} />
     </div>
   );
 };
