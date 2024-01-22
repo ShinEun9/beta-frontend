e1feebd8718286c94b663c9c34b3bdbe174879dd
ShinEun9 <ses2201@naver.com>
2024-01-22 12:48:38 +0900

    Remove: useCarouselDragStore 전역상태 제거 (#98)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,7 @@
-import useCarouselDragStore from "./useCarouselDragStore";
 import useLoginStore from "./useLoginStore";
 import useModalStore from "./useModalStore";
 import useReservationFormStore from "./useReservationFormStore";
 import useImagePreviewUrlStore from "./useImagePreviewUrlStore";
 import useImageFileStore from "./useImageFileStore";
 
-export { useCarouselDragStore, useLoginStore, useModalStore, useReservationFormStore, useImageFileStore, useImagePreviewUrlStore };
+export { useLoginStore, useModalStore, useReservationFormStore, useImageFileStore, useImagePreviewUrlStore };
