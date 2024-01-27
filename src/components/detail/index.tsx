d2ec2f80ee6d91a95d8a6da690d66aca640c715a
ebokyung <dlqhrud303@naver.com>
2024-01-27 16:29:34 +0900

    Feat: 빠른 초기 로드를 위해 페이지 컴포넌트 동적 import (lazy) 적용 (#131)

--- src/components/detail/index.tsx
+++ src/components/detail/index.tsx
@@ -2,9 +2,7 @@ import SubMenuBar from "./SubMenuBar/SubMenuBar";
 import SubMenuBarSkeleton from "./SubMenuBar/SubMenuBarSkeleton";
 import SubMenuSection from "./SubMenuSection/SubMenuSection";
 import LikeButton from "./LikeButton/LikeButton";
-import InfoSection from "./InfoSection/InfoSection";
 import InfoSectionSkeleton from "./InfoSection/InfoSectionSkeleton";
-import ReviewSection from "./ReviewSection/ReviewSection";
 import ReviewSectionSkeleton from "./ReviewSection/ReviewSectionSkeleton";
 import ReviewForm from "./ReviewForm/ReviewForm";
 import ReviewItem from "./ReviewItem/ReviewItem";
@@ -22,9 +20,7 @@ export {
   SubMenuBarSkeleton,
   SubMenuSection,
   LikeButton,
-  InfoSection,
   InfoSectionSkeleton,
-  ReviewSection,
   ReviewSectionSkeleton,
   ReviewForm,
   ReviewItem,
