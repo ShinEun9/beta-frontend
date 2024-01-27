303333374787f249a1c766e8a39993cc75ef19db
ebokyung <dlqhrud303@naver.com>
2024-01-27 16:33:35 +0900

    Feat: index chunk에서 node_modules 분리 (vendor chunk 생성) (#131)

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") })],
+  plugins: [
+    react(),
+    svgr(),
+    ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") }),
+    splitVendorChunkPlugin(),
+    visualizer({
+      filename: "./dist/report.html",
+      open: true,
+      gzipSize: true,
+    }),
+  ],
   server: {
-    port: 3000,
+    port: 4173,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:5000",
