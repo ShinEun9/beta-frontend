b834cf6762d5a117facf2b56a2ee2582d82ca16b
윤태현 <yoonth0919@gmail.com>
2024-01-29 17:27:02 +0900

    Refactor: 상세 페이지 배너 이미지 컴포넌트 화 (#139)

--- src/components/detail/Banner/Banner.tsx
+++ src/components/detail/Banner/Banner.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
-import { Carousel } from "@/components/common";
+import { BannerImage, Carousel } from "@/components/common";
 import { ShowType } from "@/types";
 import styles from "./Banner.module.css";
 import { useLoginStore } from "@/stores";
@@ -13,14 +13,15 @@ const Banner = () => {
   } = useLoginStore();
 
   const { main_image_url: mainImg, sub_images_url } = queryClient.getQueryData<ShowType>(["infoData", showId, login_id])!;
-  const subImgs = sub_images_url ? Object.values(JSON.parse(sub_images_url)) : [];
-  const bannerImgs = [mainImg, ...subImgs];
+  const subImgs: string[] = sub_images_url ? Object.values(JSON.parse(sub_images_url)) : [];
+  const bannerImgs: string[] = [mainImg, ...subImgs];
 
   return (
     <Carousel index={0} dataLength={bannerImgs.length}>
       {bannerImgs.map((img, index) => (
         <div key={index}>
-          <img src={import.meta.env.VITE_APP_IMAGE_DOMAIN + img} className={styles["slider__img"]} />
+          <BannerImage imgUrl={img} page="detail" />
+          {/* <img src={import.meta.env.VITE_APP_IMAGE_DOMAIN + img} className={styles["slider__img"]} /> */}
         </div>
       ))}
     </Carousel>
