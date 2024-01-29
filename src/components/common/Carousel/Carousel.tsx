47356f132f2abe55abd16c585f26805ac56203a8
ShinEun9 <ses2201@naver.com>
2024-01-29 10:28:10 +0900

    Refactor: 메인페이지 배너이미지 갯수가 default slidesToShow보다 작을 때 dummy image 보여주기 위한 리팩토링(#4)

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Slider from "react-slick";
 import { useCarouselDragStore } from "@/stores";
 import nextArrow from "@/assets/next-arrow.png";
 import prevArrow from "@/assets/prev-arrow.png";
+import DummyBannerImage from "@/assets/dummy-banner-img.svg?react";
 import "./slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./Carousel.module.css";
@@ -10,6 +11,7 @@ import styles from "./Carousel.module.css";
 interface PropsType {
   index: number;
   initialSlide?: number;
+  dataLength?: number;
   children: React.ReactNode;
 }
 
@@ -54,7 +56,7 @@ const settings = [
     swipeToSlide: true,
     responsive: [
       {
-        breakpoint: 500,
+        breakpoint: 450,
         settings: {
           slidesToShow: 1,
         },
@@ -101,40 +103,20 @@ const settings = [
       },
     ],
   },
-  {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    appendDots: (dots: never) => renderCustomDots(dots),
-    dotsClass: "dots",
-    slidesToShow: 3,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 760,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-    nextArrow: <NextArrows />,
-    prevArrow: <PrevArrows />,
-  },
 ];
 
-const Carousel: React.FC<PropsType> = ({ index, initialSlide = 0, children }) => {
+const Carousel: React.FC<PropsType> = ({ index, initialSlide = 0, children, dataLength }) => {
   const { setIsDragging } = useCarouselDragStore();
+
+  const calculatedSettings = useMemo(() => {
+    if (index === 0 && dataLength! <= 2) {
+      return { ...settings[0], autoplay: false };
+    } else return settings[index];
+  }, [index, dataLength]);
 
   return (
     <Slider
-      {...settings[index]}
+      {...calculatedSettings}
       initialSlide={initialSlide}
       beforeChange={() => {
         setIsDragging(true);
@@ -144,6 +126,12 @@ const Carousel: React.FC<PropsType> = ({ index, initialSlide = 0, children }) =>
       }}
     >
       {children}
+      {dataLength === 1 && (
+        <div className={styles["dummy-image"]}>
+          <DummyBannerImage />
+          <p>게시물을 기다리고 있어요</p>
+        </div>
+      )}
     </Slider>
   );
 };
