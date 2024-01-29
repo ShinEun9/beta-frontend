eb5f02b4412414081b914536ffab2c5ed57ee044
ShinEun9 <ses2201@naver.com>
2024-01-29 23:34:07 +0900

    Add: 메인페이지 에러바운더리 추가 (#2)

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,12 +1,15 @@
 import { BannerSection, StorySection, CalendarSection } from "@/components/main";
 import "react-loading-skeleton/dist/skeleton.css";
+import { ErrorBoundary } from "react-error-boundary";
 
 const MainPage = () => {
   return (
     <main>
-      <BannerSection />
-      <StorySection />
-      <CalendarSection />
+      <ErrorBoundary fallback={<div>에러페이지</div>}>
+        <BannerSection />
+        <StorySection />
+        <CalendarSection />
+      </ErrorBoundary>
     </main>
   );
 };
