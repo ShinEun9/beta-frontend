3dcdb38e61d57c265292ad7a51e51c5a98e00095
ShinEun9 <ses2201@naver.com>
2024-01-29 17:34:16 +0900

    Refactor: 메인공연/전시페이지 필터값 url queryString에 저장 및 필터 버튼 클릭 시 queryString 변경 구현 (#20)

--- src/pages/MainConcert/MainConcertPage.tsx
+++ src/pages/MainConcert/MainConcertPage.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import queryString from "query-string";
 import { Filters } from "@/components/common";
 import { ConcertListSection } from "@/components/mainConcert";
 import { getTodayStringDate } from "@/utils";
-import { ShowFilterRequestType } from "@/types";
 
 const MainConcertPage = () => {
   const { todayString } = getTodayStringDate();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const queries = searchParams.toString();
 
-  const [filterRequest, setFilterRequest] = useState<ShowFilterRequestType>({
-    start_date: todayString,
-    end_date: todayString,
-    location: "all",
-    progress: "1",
-    category: "all",
-  });
+  useEffect(() => {
+    const { location, start_date, end_date, category, progress, date } = queryString.parse(queries);
+
+    if (!location || !start_date || !end_date || !category || !progress || !date) {
+      navigate({ search: `?location=all&start_date=${todayString}&end_date=${todayString}&category=all&progress=1&date=오늘` });
+    }
+  }, [searchParams]);
 
   return (
     <main>
-      <Filters filterRequest={filterRequest} setFilterRequest={setFilterRequest} />
-      <ConcertListSection filterRequest={filterRequest} />
+      <Filters />
+      <ConcertListSection />
     </main>
   );
 };
