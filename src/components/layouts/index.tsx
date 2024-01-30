b093f12439c9aee1c2b9d78f569d113833e94ac1
ShinEun9 <ses2201@naver.com>
2024-01-30 23:45:49 +0900

    Refactor: 헤더 css 변경으로 인한 리팩토링 (#4)

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,5 +1,9 @@
 import Header from "./Header/Header";
 import NavBar from "./NavBar/NavBar";
+import SideNavBar from "./SideNavBar/SideNavBar";
+import MypageSideNavBar from "./MypageSideNavBar/MypageSideNavBar";
 import MypageNavBar from "./MypageNavBar/MypageNavBar";
+import ButtonBeforeLogin from "./ButtonBeforeLogin/ButtonBeforeLogin";
+import ButtonAfterLogin from "./ButtonAfterLogin/ButtonAfterLogin";
 
-export { Header, NavBar, MypageNavBar };
+export { Header, NavBar, SideNavBar, MypageSideNavBar, MypageNavBar, ButtonBeforeLogin, ButtonAfterLogin };
