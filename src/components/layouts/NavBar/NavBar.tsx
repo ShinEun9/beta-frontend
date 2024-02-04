c298d374013a13a31bc132f54cb641fe4a3ea0ff
ShinEun9 <ses2201@naver.com>
2024-02-04 18:06:04 +0900

    Fix: 메인공연/전시페이지에서 뒤로가기버튼 눌러도 뒤로가지 않는 버그 해결 (#20)

--- src/components/layouts/NavBar/NavBar.tsx
+++ src/components/layouts/NavBar/NavBar.tsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./NavBar.module.css";
+import { getTodayStringDate } from "@/utils";
 
 const cx = classNames.bind(styles);
 
-const navList = [
-  { pathname: "/concert", text: "공연" },
-  { pathname: "/exhibition", text: "전시" },
-  // { pathname: "/sports", text: "스포츠" },
-];
-
 const NavBar = () => {
+  const { todayString } = getTodayStringDate();
+  const navList = [
+    { pathname: "/concert", text: "공연" },
+    { pathname: "/exhibition", text: "전시" },
+  ];
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles["navbar-wrapper"]}>
         {navList.map((item) => (
           <li key={item.pathname} className={cx("navbar__item", location.pathname === item.pathname && "selected")}>
-            <Link to={item.pathname}>{item.text}</Link>
+            <Link
+              to={{
+                pathname: item.pathname,
+                search: `?location=all&start_date=${todayString}&end_date=${todayString}&progress=1&date=오늘${
+                  item.pathname === "/concert" ? "&category=all" : ""
+                }`,
+              }}
+            >
+              {item.text}
+            </Link>
           </li>
         ))}
       </ul>
--- src/pages/MainExhibition/MainExhibitionPage.tsx
+++ src/pages/MainExhibition/MainExhibitionPage.tsx
@@ -1,23 +1,7 @@
-import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import queryString from "query-string";
 import { Filters } from "@/components/common";
 import { ExhibitionListSection } from "@/components/mainExhibition";
-import { getTodayStringDate } from "@/utils";
 
 const MainExhibitionPage = () => {
-  const { todayString } = getTodayStringDate();
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
-  const queries = searchParams.toString();
-
-  useEffect(() => {
-    const { location, start_date, end_date, progress, date } = queryString.parse(queries);
-    if (!location || !start_date || !end_date || !progress || !date) {
-      navigate({ search: `?location=all&start_date=${todayString}&end_date=${todayString}&progress=1&date=오늘` });
-    }
-  }, [searchParams]);
-
   return (
     <main>
       <Filters />
